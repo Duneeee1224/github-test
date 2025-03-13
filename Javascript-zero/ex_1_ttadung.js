51a19ab93c483220b141410d7c4874993a1766a1
Duneeee1224 <trantheanhdung2412@gmail.com>
2025-03-13 11:04:44 +0700

    Add student object example

---
+++ Javascript-zero/ex_1_ttadung.js
@@ -0,0 +1,16 @@
+// định nghĩa 1 object student, có 3 key
+// name: string
+// age: number
+// detail là 1 object chứa
+// height: number
+// phone: number
+const student = {
+    name: "Nguyen Van A",  // string
+    age: 22,               // number
+    detail: {
+        height: 170,       // number
+        phone: "0987654321"  // number
+    }
+};
+
+console.log(student);
